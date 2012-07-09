2801acc2778aa842b280774393592141c211f51a
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@spamtrap.com>
2012-07-09 13:50:16 +0100

    Fixed failed invocation of clang build

--- pthreads Notifer Object/withclang.sh
+++ pthreads Notifer Object/withclang.sh
@@ -1,5 +1,6 @@
-clang -std=c++11 -o unittests -DUSE_PARALLEL pthread_permit.c unittests.cpp -lrt -ltbb
+cp pthread_permit.c pthread_permit.cpp
+clang -std=c++11 -o unittests -DUSE_PARALLEL pthread_permit.cpp unittests.cpp -lrt -ltbb
 if [ "$?" != "0" ]; then
-  clang -std=c++11 -o unittests pthread_permit.c unittests.cpp -lrt
+  clang -std=c++11 -o unittests pthread_permit.cpp unittests.cpp -lrt
 fi
-clang -std=c++11 -o pthread_permit_speedtest pthread_permit.c pthread_permit_speedtest.cpp -lrt
+clang -std=c++11 -o pthread_permit_speedtest pthread_permit.cpp pthread_permit_speedtest.cpp -lrt
